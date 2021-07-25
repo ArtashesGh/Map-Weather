dedcd561cc75bb0aa99812d278cb4e1a5138a397
Artashesgh <artashes.gh@gmail.com>
2021-07-25 15:15:47 +0400

    Initial Commit

---
+++ Map Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Map Weather
+//
+//  Created by Artashes Noknok on 7/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
